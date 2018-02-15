8e8656bcd33364209c51aed4afb7bbb845903274
benjaminmballa <mballabenjamin@yahoo.fr>
2018-02-15 10:40:04 +0100

    Initial Commit

---
+++ ios-project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-project
+//
+//  Created by Efrei Paris on 15/02/2018.
+//  Copyright © 2018 Efrei Paris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
